89706ec9c4da0bc2d75f16f65291776ecd78fa2b
lihuazhang <lihuazhang@hotmail.com>
2016-12-04 13:55:07 +0800

    更新

--- ruby-china-ios/Constants.swift
+++ ruby-china-ios/Constants.swift
@@ -14,7 +14,7 @@ let COPYRIGHT_URL = "https://github.com/ruby-china/ruby-china-ios/blob/master/LI
 let PROJECT_URL = "https://github.com/ruby-china/ruby-china-ios"
 
 let APP_VERSION = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-let USER_AGENT = "turbolinks-app, ruby-china, official, iOS, version:" + APP_VERSION
+let USER_AGENT = "turbolinks-app, testerhome, official, iOS, version:" + APP_VERSION
 
 // Blue Theme
 //let NAVBAR_BG_COLOR = UIColor(red:0.25, green:0.32, blue:0.71, alpha:1.0)
@@ -27,8 +27,8 @@ let USER_AGENT = "turbolinks-app, ruby-china, official, iOS, version:" + APP_VER
 let BLACK_COLOR = UIColor(red: 0.04, green: 0.02, blue: 0.02, alpha: 1.0)
 let PRIMARY_COLOR = UIColor(red: 0.91, green: 0.33, blue: 0.23, alpha: 1.0)
 //let NAVBAR_BG_COLOR = PRIMARY_COLOR
-let SIDEMENU_NAVBAR_BG_COLOR = UIColor(red: 0.74, green: 0.24, blue: 0.13, alpha: 1.0)
-let SIDEMENU_BG_COLOR = UIColor(red: 0.95, green: 0.94, blue: 0.94, alpha: 1.0)
+//let SIDEMENU_NAVBAR_BG_COLOR = UIColor(red: 0.74, green: 0.24, blue: 0.13, alpha: 1.0)
+//let SIDEMENU_BG_COLOR = UIColor(red: 0.95, green: 0.94, blue: 0.94, alpha: 1.0)
 //let NAVBAR_BORDER_COLOR = UIColor(red: 0.72, green: 0.30, blue: 0.26, alpha: 1.0)
 //let NAVBAR_TINT_COLOR = UIColor(red: 1.00, green: 1.00, blue: 0.98, alpha: 1.0)
 //let SEGMENT_BG_COLOR = UIColor(red: 0.23, green: 0.05, blue: 0.02, alpha: 1.0)
@@ -40,6 +40,8 @@ let NAVBAR_BORDER_COLOR = UIColor(red:0.25, green:0.32, blue:0.61, alpha:1.0)
 let NAVBAR_TINT_COLOR = UIColor(red:1.00, green:1.00, blue:0.93, alpha:1.0)
 let SEGMENT_BG_COLOR = UIColor(red:0.10, green:0.14, blue:0.39, alpha:1.0)
 let TABBAR_BG_COLOR = UIColor(red:0.88, green:0.96, blue:1.00, alpha:1.0)
+let SIDEMENU_NAVBAR_BG_COLOR = UIColor(red:0.25, green:0.32, blue:0.71, alpha:1.0)
+let SIDEMENU_BG_COLOR = UIColor(red: 0.95, green: 0.94, blue: 0.94, alpha: 1.0)
 
 // - 消息定义
 /// 显示侧栏
